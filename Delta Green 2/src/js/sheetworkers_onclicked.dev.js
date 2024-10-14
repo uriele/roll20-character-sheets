370ef123cf46046fe9ea2863c6ac5b9f0275e20d
uriele <menarini.marco@gmail.com>
2024-10-14 11:11:34 +0200

    all

--- Delta Green 2/src/js/sheetworkers_onclicked.dev.js
+++ Delta Green 2/src/js/sheetworkers_onclicked.dev.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
